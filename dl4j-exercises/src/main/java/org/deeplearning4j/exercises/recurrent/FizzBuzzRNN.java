e141a58abdb41bd347f79789a90070d3ab080337
eraly <susan.eraly@gmail.com>
2016-10-14 17:43:09 +0530

    links to basic rnn

--- dl4j-exercises/src/main/java/org/deeplearning4j/exercises/recurrent/FizzBuzzRNN.java
+++ dl4j-exercises/src/main/java/org/deeplearning4j/exercises/recurrent/FizzBuzzRNN.java
@@ -1,14 +1,15 @@
 package org.deeplearning4j.exercises.recurrent;
 
 /**
- * This example trains a RNN. WHen trained we only have to put the first
- * character of LEARNSTRING to the RNN, and it will recite the following chars
+ * This example trains a RNN to memorize the fizzbuzz sequence.
+ * Frame 1-15 as a sequence
+ * How would you represent your input vectors here?
  *
- * @author Peter Grossmann
  */
 public class FizzBuzzRNN {
 
-    //Need to write
+    // You can use this example for reference which is very similar (memorize a string)
+	// https://github.com/deeplearning4j/dl4j-examples/blob/master/dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/basic/BasicRNNExample.java
 
 
 }
